045acceba6a3ee9188c88cb03135d5f67c5ccbf3
Ismail Rohaga <azh.zhafir@gmail.com>
2017-12-11 19:38:47 +0700

    Final
    next!

--- app/src/main/java/com/preangerstd/hellokotlin/MainActivity.kt
+++ app/src/main/java/com/preangerstd/hellokotlin/MainActivity.kt
@@ -2,11 +2,22 @@ package com.preangerstd.hellokotlin
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button.setText("Click Me")
+
+        button.setOnClickListener {
+
+            textView.setText("Hello Kotlin!")
+            Toast.makeText(this,"Kotlin Start Here!",Toast.LENGTH_LONG).show()
+
+        }
     }
 }
